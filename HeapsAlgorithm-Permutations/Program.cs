aa1888e54158f5d1b4384abd8e820f77e54a3757
sam-klok <sklok11@gmail.com>
2021-10-12 15:34:38 -0700

    Initial commit

---
+++ HeapsAlgorithm-Permutations/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HeapsAlgorithm_Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var all = new List<List<int>>();
+
+            // initialize
+            string str = "1234";
+            int[] arr = str.Select(c => c - '0').ToArray();
+
+            GetPer(arr, all);
+
+
+            PrintAll(all);
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            if (a == b) return;
+
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void GetPer(int[] a, List<List<int>> all)
+        {
+            //int x = a.Length - 1;
+            int x = a.Length - 1;
+            GetPer(a.ToArray(), 0, x, all);
+        }
+
+        private static void GetPer(int[] a, int k, int m, List<List<int>> all)
+        {
+            if (k == m)
+            {
+                //Console.Write(list);
+                //Console.WriteLine();
+
+                List<int> l = new List<int>();
+                l.AddRange(a);
+                all.Add(l);
+
+            }
+            else
+                for (int i = k; i <= m; i++)
+                {
+                    Swap(ref a[k], ref a[i]);
+                    GetPer(a, k + 1, m, all);
+                    Swap(ref a[k], ref a[i]);
+                }
+        }
+
+        private static void PrintAll(List<List<int>> a)
+        {
+            int counter = 1;
+            foreach (var l in a)
+            {
+                Console.Write($"{counter,4} - ");
+
+                foreach (var c in l)
+                    Console.Write(c);
+
+                Console.WriteLine();
+                counter++;
+            }
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,23 @@
+Heap's algorithm generates all possible permutations of n objects.
+Let's do it for integers.
+
+Collection:    1, 2, 3
+Permutations: {1, 2, 3}
+              {1, 3, 2}
+              {2, 1, 3}
+              {2, 3, 1}
+              {3, 1, 2}
+              {3, 2, 1}
+
+
+Theoretical explanation:
+https://en.wikipedia.org/wiki/Heap%27s_algorithm
+
+Practical explanations:
+https://stackoverflow.com/questions/11208446/generating-permutations-of-a-set-most-efficiently
+
+Good example for char objects:
+https://stackoverflow.com/questions/756055/listing-all-permutations-of-a-string-integer
+
+thank you,
+Sam Klok
